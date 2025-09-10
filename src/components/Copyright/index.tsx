21cfd4a12efd39e07d54d9a1c23de0cfa708e253
fatihmuhamadridho <fatih.ridho@salt.co.id>
2025-09-10 23:40:00 +0700

    feat: 🎸 pearson correlations data

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Text } from '@mantine/core';
+import React from 'react';
+
+const Copyright = () => {
+  return (
+    <Container py={12}>
+      <Text fz={12} ta={'center'}>
+        @Copyright by fatihmuhamadridho
+      </Text>
+    </Container>
+  );
+};
+
+export default Copyright;
---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,70 @@
+import { Center, SimpleGrid, Table, Text } from '@mantine/core';
+import React from 'react';
+
+export interface tableHeadersProps {
+  label?: string;
+  key?: any;
+  width?: number;
+}
+
+export interface DataTablePrpos {
+  header: tableHeadersProps[];
+  data?: any[];
+  width?: any;
+  mah?: number | string;
+}
+
+const DataTable = (props: DataTablePrpos) => {
+  const { header, data, mah } = props;
+  return (
+    <SimpleGrid>
+      <Table.ScrollContainer className="break-words overflow-hidden" minWidth={'100%'} mah={mah || 550}>
+        <Table striped stickyHeader stickyHeaderOffset={-0.3} highlightOnHover withTableBorder withColumnBorders>
+          <Table.Thead className="z-[5]">
+            <Table.Tr>
+              {header?.map((headData, headIndex: number) => {
+                return <Table.Th key={headIndex}>{headData?.label}</Table.Th>;
+              })}
+            </Table.Tr>
+          </Table.Thead>
+          {data && data?.length > 0 && (
+            <Table.Tbody>
+              {data?.map((itemData: any, itemIndex: number) => {
+                return (
+                  <Table.Tr key={itemIndex}>
+                    {header.map((headData, headIndex: number) => {
+                      if (headData?.key === 'index')
+                        return (
+                          <Table.Td className="text-center" miw={headData?.width || 100} key={headIndex}>
+                            {itemIndex + 1}
+                          </Table.Td>
+                        );
+                      if (typeof headData?.key === 'function')
+                        return (
+                          <Table.Td className="align-top" miw={headData?.width || 100} key={headIndex}>
+                            {headData?.key(itemData, itemIndex)}
+                          </Table.Td>
+                        );
+                      return (
+                        <Table.Td className="align-top" miw={headData?.width || 100} key={headIndex}>
+                          {itemData?.[headData?.key]}
+                        </Table.Td>
+                      );
+                    })}
+                  </Table.Tr>
+                );
+              })}
+            </Table.Tbody>
+          )}
+        </Table>
+        {(!data || data?.length < 1) && (
+          <Center className="border" w={'100%'} h={'20vh'}>
+            <Text>Data Kosong</Text>
+          </Center>
+        )}
+      </Table.ScrollContainer>
+    </SimpleGrid>
+  );
+};
+
+export default DataTable;
